c1515d0656acd453f5d79b51927d636e57a5054f
raccoon <minsungt@naver.com>
2022-12-22 03:03:54 +0900

    sdf

--- AI_python.py
+++ AI_python.py
@@ -11,21 +11,8 @@ import torchvision.utils as vutils
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 
-import glob
-from PIL import Image
-import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
-from torchvision.io import read_image
-import torch.nn as nn
-import matplotlib.pyplot as plt
-import random
-import torchvision
-import torchvision.transforms as transforms
-
 import os
 import numpy as np
-Image_Path = './download/image_color.jpeg'
-Image_Gray_Path = './download/image.jpeg'
 
 def toYUV(rgb):
     rgb = rgb.numpy()
@@ -33,7 +20,7 @@ def toYUV(rgb):
     Y = 0.2126 * R + 0.7152 * G + 0.0722 *B
     U = -0.09991 * R + -0.33609 * G + 0.436 * G
     V = 0.615 * R + -0.55861 * G - 0.05639 * B
-    return torch.from_numpy(np.asarray([Y, U, V]).reshape(3, 45, 80))
+    return torch.from_numpy(np.asarray([Y, U, V]).reshape(3, 64, 64))
 def toRGB(yuv, batchsize):
     """shape of yuv is bs x 3 x 64 x 64, ordered by YUV"""
     lst = []
@@ -43,16 +30,44 @@ def toRGB(yuv, batchsize):
         G = Y - 0.21482 * U - 0.38059 * V
         B = Y + 2.12798 * U
         lst.append([R,G,B])
-    return np.asarray(lst).reshape(batchsize, 3, 45, 80)#.clip(0, 255)
+    return np.asarray(lst).reshape(batchsize, 3, 64, 64)#.clip(0, 255)
 
+def make_file_list():
+    train_img_list = list()
+    for img_idx in range(14):
+        img_path = "./dataset/"+ str(img_idx + 1) + ".jpeg"
+        train_img_list.append(img_path)
+    return train_img_list
 
-transform = transforms.Compose([
-    transforms.Resize((45,80)),
-    transforms.ToTensor()
-])
-img = Image.open(Image_Path)
-train_data = transform(img)
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=1)
+class Img_Dataset(Dataset):
+    def __init__(self, file_list, transform):
+        self.file_list = file_list
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        img_path = self.file_list[index]
+        img = Image.open(img_path)
+        img_transformed = self.transform(img)
+        return img_transformed
+
+class ImageTransform():
+
+    def __init__(self):
+        self.data_transform = transforms.Compose([
+            transforms.Resize((45,80)),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: toYUV(x))
+        ])
+
+    def __call__(self, img):
+        return self.data_transform(img)
+
+train_dataset = Img_Dataset(file_list=make_file_list(),
+                            transform=ImageTransform())
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1)
 device = torch.device('mps:0' if torch.backends.mps.is_available() else 'cpu')
 batchSize = 1
 
@@ -61,7 +76,7 @@ def extractGray(batchSize, yuv):
     lst = []
     for data in yuv:
         lst.append(data[0])
-    return np.asarray(lst).reshape(batchSize, 1, 45, 80)
+    return np.asarray(lst).reshape(batchSize, 1, 64, 64)
 
 
 class _netD(nn.Module):
@@ -95,13 +110,13 @@ class _netD(nn.Module):
             # 512 x 5 x 2
         )
         self.fc = nn.Sequential(
-            nn.Linear(512 * 2 * 5, 1),
+            nn.Linear(512 * 4 * 4, 1),
             nn.Sigmoid()
         )
     def forward(self, input):
         # input is real or fake colored image
         x = self.cnn(input)
-        x = x.view(x.size(0), 512 * 2 * 5) # flatten it
+        x = x.view(x.size(0), 512 * 4 * 4) # flatten it
         output = self.fc(x)
         return output.view(-1,1).squeeze(1)
 
@@ -110,7 +125,7 @@ class _netG(nn.Module):
     def __init__(self):
         super(_netG, self).__init__()
 
-        self.fc = nn.Linear(100, 1 * 45 * 80)
+        self.fc = nn.Linear(100, 1 * 64 * 64)
         self.conv1 = nn.Conv2d(2, 126, 3, 1, 1, bias=False)#130
         self.bn1 = nn.BatchNorm2d(126)#130
 
@@ -132,7 +147,7 @@ class _netG(nn.Module):
     def forward(self, input, noise_pure):
         # input is grayscale image(Y of YUV), noise is random sampled noise
         noise = self.fc(noise_pure)
-        noise = noise.view(noise.size(0), 1, 45, 80)
+        noise = noise.view(noise.size(0), 1, 64, 64)
 
         # 2 x 64 x 64
         x = self.conv1(torch.cat([input, noise], dim=1))
@@ -208,58 +223,62 @@ netD_PATH = './results/netD.pth'
 netG = _netG()
 netG.load_state_dict(torch.load(netG_PATH))
 netG.eval()
+netG.to(device)
 
 netD = _netD()
 netD.load_state_dict(torch.load(netD_PATH))
 netD.eval()
+netD.to(device)
 
-data = train_data
-data = data.to(device)
-batchSize = len(data)
-gray_data = train_data.to(device)
-gray = gray_data.cpu().numpy()
-grayv = Variable(torch.from_numpy(gray)).to(device)
+for i, data in enumerate(train_loader, 0):
+    data = data.to(device)
+    batchSize = len(data)
+    gray = extractGray(batchSize, data.cpu().numpy())
+    grayv = Variable(torch.from_numpy(gray)).to(device)
         #############
         # D!        #
         #############
-netD.zero_grad()
+    netD.zero_grad()
         ##############
         # real image #
         ##############
-input.resize_as_(data).copy_(data)
-label.resize_(len(data)).fill_(real_label)
-inputv = Variable(input).to(device)
-labelv = Variable(label).to(device)
+    input.resize_as_(data).copy_(data)
+    label.resize_(len(data)).fill_(real_label)
+    inputv = Variable(input).to(device)
+    labelv = Variable(label).to(device)
 
-output = netD(inputv)
-D_x = output.data.mean()
+    output = netD(inputv)
+    D_x = output.data.mean()
 
         ##############
         # fake image #
         ##############
-noise.resize_(batchSize, 100).uniform_(0,1)
-noisev = Variable(noise).to(device)
+    noise.resize_(batchSize, 100).uniform_(0,1)
+    noisev = Variable(noise).to(device)
 
         # create fake images
-fake = netG(grayv, noisev)
-
+    fake = netG(grayv, noisev)
+    fake.size()
         # cal loss
-output = netD(fake.detach())
-labelv = Variable(label.fill_(fake_label)).to(device)
-D_G_z1 = output.data.mean()
-
-optimizerD.step()
+    output = netD(fake.detach())
+    labelv = Variable(label.fill_(fake_label)).to(device)
+    D_G_z1 = output.data.mean()
+    
+    optimizerD.step()
 
         ##############
         # G!         #
         ##############
-netG.zero_grad()
-labelv = Variable(label.fill_(real_label)).to(device)
-output = netD(fake)
+    netG.zero_grad()
+    labelv = Variable(label.fill_(real_label)).to(device)
+    output = netD(fake)
+        
+    errG = criterion(output, labelv)
+    errG.backward()
+    D_G_z2 = output.data.mean()
+    optimizerG.step()
 
-D_G_z2 = output.data.mean()
-optimizerG.step()
-
-rgb = toRGB(fake.cpu().data.numpy(), batchSize)
-real = toRGB(data.cpu().data.numpy(), batchSize)
-vutils.save_image(torch.from_numpy(rgb),'%s/fake_samples_epoch_%s.png' % (outf, epoch))
+    
+    rgb = toRGB(fake.cpu().data.numpy(), batchSize)
+    real = toRGB(data.cpu().data.numpy(), batchSize)
+    vutils.save_image(torch.from_numpy(rgb),'%s/fake_samples_epoch_%s.png' % (outf, i))
